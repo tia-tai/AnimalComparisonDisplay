f7587e25e25821adc5a722234cb3c92a52c8c28f
tia-tai <taiyulin14@gmail.com>
2025-12-02 14:33:28 -0500

    added in animal data

---
+++ src/main/java/com/example/animalcomparisondisplay/Animals.java
@@ -0,0 +1,82 @@
+package com.example.animalcomparisondisplay;
+
+import java.util.ArrayList;
+
+public class Animals {
+    private int rank;
+    private String name;
+    private float length;
+    private int maxMass;
+    static ArrayList<Animals> animals = new ArrayList<>();
+
+    public Animals(int rank, String name, float length, int maxMass) {
+        this.rank = rank;
+        this.name = name;
+        this.length = length;
+        this.maxMass = maxMass;
+        animals.add(this);
+    }
+
+    public static ArrayList<Animals> getAnimals() {
+        return animals;
+    }
+
+    public static void setAnimals(ArrayList<Animals> animals) {
+        Animals.animals = animals;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public int getMaxMass() {
+        return maxMass;
+    }
+
+    public void setMaxMass(int maxMass) {
+        this.maxMass = maxMass;
+    }
+
+    public String toString() {
+        return getName() + " has a max mass of " + getMaxMass() + "lb";
+    }
+
+    public static void summarize() {
+        float avgLength = 0f;
+        int avgMaxMass = 0;
+
+        for (Animals animal: getAnimals()) {
+            avgLength += animal.getLength();
+            avgMaxMass += animal.getMaxMass();
+        }
+
+        System.out.println("\nThere are " + getAnimals().size() + " animals in the dataset");
+
+        System.out.println("The total length of the animals is " + avgLength + " feet\n" + "The total max mass of the animals is " + avgMaxMass + " lbs");
+
+        avgLength = avgLength / getAnimals().size();
+        avgMaxMass = avgMaxMass / getAnimals().size();
+
+        System.out.println("\nThe average length of the animals is " + avgLength + " feet\n" + "The average max mass of the animals is " + avgMaxMass + " lbs");
+    }
+}
---
+++ src/main/java/com/example/animalcomparisondisplay/Mammal.java
@@ -0,0 +1,58 @@
+package com.example.animalcomparisondisplay;
+
+import com.example.animalcomparisondisplay.Animals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mammal extends Animals {
+    private int avgMass;
+    static ArrayList<Mammal> mammals = new ArrayList<Mammal>();
+
+    public Mammal(int rank, String name, float length, int maxMass, int avgMass) {
+        super(rank, name, length, maxMass);
+        this.avgMass = avgMass;
+        mammals.add(this);
+    }
+
+    public static ArrayList<Mammal> getMammals() {
+        return mammals;
+    }
+
+    public static void setMammals(ArrayList<Mammal> mammals) {
+        Mammal.mammals = mammals;
+    }
+
+    public long getAvgMass() {
+        return avgMass;
+    }
+
+    public void setAvgMass(int avgMass) {
+        this.avgMass = avgMass;
+    }
+
+    public String toString() {
+        return getName() + " has a max mass of " + getMaxMass() + "lb";
+    }
+
+    public static void readMammalData() throws Exception {
+        File dataFile = new File("MammalData");
+        Scanner fileScanner = new Scanner(dataFile);
+        while (fileScanner.hasNextLine()) {
+            String data = fileScanner.nextLine();
+            String[] dataSegment = data.split("\t");
+            if (dataSegment[0].equals("Rank") || dataSegment[0].split(" ")[0].equals("in")) {
+                System.out.println("Line Skipped");
+            } else {
+                int rank = Integer.parseInt(dataSegment[0]);
+                String name = dataSegment[1].split("\\[")[0];
+                float length = Float.parseFloat(dataSegment[4].split("\\(")[1].split("\\)")[0]);
+                int maxMass = (int) Float.parseFloat(dataSegment[3].split("\\[")[0])*2205;
+                int avgMass = (int) Float.parseFloat(dataSegment[2].split("\\[")[0])*2205;
+                new Mammal(rank, name, length, maxMass, avgMass);
+            }
+        }
+    }
+}
