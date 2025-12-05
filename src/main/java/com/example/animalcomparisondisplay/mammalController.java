edb3e14d36634d1a48bfb13ac2a14800c9809bdb
tia-tai <taiyulin14@gmail.com>
2025-12-05 10:30:26 -0500

    displaying data

--- src/main/java/com/example/animalcomparisondisplay/mammalController.java
+++ src/main/java/com/example/animalcomparisondisplay/mammalController.java
@@ -2,6 +2,7 @@ package com.example.animalcomparisondisplay;
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class mammalController {
@@ -13,12 +14,23 @@ public class mammalController {
     public TableColumn<Mammal, Integer> maxMassTable;
     public TableColumn<Mammal, Float> avgLengthTable;
 
+    public TextField currentRank;
+    public TextField currentName;
+    public TextField currentAvgMass;
+    public TextField currentAvgLength;
+    public TextField currentMaxMass;
+
     public void initialize()  {
         try {
             Mammal.readMammalData();
+            int x = 0;
 
             for (Mammal mammal : Mammal.getMammals()) {
                 mammalDatatable.getItems().add(mammal);
+                if (x==0) {
+                    mammalDatatable.getSelectionModel().select(mammal);
+                    x++;
+                }
             }
 
             rankTable.setCellValueFactory(new PropertyValueFactory<>("rank"));
@@ -26,9 +38,24 @@ public class mammalController {
             avgMassTable.setCellValueFactory(new PropertyValueFactory<>("avgMass"));
             maxMassTable.setCellValueFactory(new PropertyValueFactory<>("maxMass"));
             avgLengthTable.setCellValueFactory(new PropertyValueFactory<>("length"));
+
+            Mammal currentMammal = mammalDatatable.getSelectionModel().getSelectedItem();
+            currentRank.setText(Integer.toOctalString(currentMammal.getRank()));
+            currentName.setText(currentMammal.getName());
+            currentAvgLength.setText(Float.toString(currentMammal.getLength()));
+            currentAvgMass.setText(Integer.toString(currentMammal.getAvgMass()));
+            currentMaxMass.setText(Integer.toString(currentMammal.getMaxMass()));
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
+    public void displayData() {
+        Mammal currentMammal = mammalDatatable.getSelectionModel().getSelectedItem();
+        currentRank.setText(Integer.toOctalString(currentMammal.getRank()));
+        currentName.setText(currentMammal.getName());
+        currentAvgLength.setText(Float.toString(currentMammal.getLength()));
+        currentAvgMass.setText(Integer.toString(currentMammal.getAvgMass()));
+        currentMaxMass.setText(Integer.toString(currentMammal.getMaxMass()));
+    }
 }
